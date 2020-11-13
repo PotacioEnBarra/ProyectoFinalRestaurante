a355c8c04e8a765a39cb00ad956fca36aa1b7512
duvan051 <dfernandomlopez@hotmail.com>
2020-11-13 14:01:43 -0500

    Modulo Usuario
    Se agrega el modulo usuario con un componente que es el registro-usuario

---
+++ src/app/modules/usuario/usuario.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+
+
+
+@NgModule({
+  declarations: [RegistroUsuarioComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsuarioModule { }
