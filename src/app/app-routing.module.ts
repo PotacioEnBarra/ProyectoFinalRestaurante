7cc745ba262570b4b3e7f77d2efd986fc5f0f9b8
Carlos Manchego <carlosm.manchegop@konradlorenz.edu.co>
2020-11-14 10:21:01 -0500

    Agregacion de repartidor
    Se ha agregado el repartidor al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NuevoProductoComponent } from './modules/productos/nuevo-producto/nuevo-producto.component';
+import { RegistrarRepartidorComponent } from './modules/repartidor/registrar-repartidor/registrar-repartidor.component';
+import { RegistroUsuarioComponent } from './modules/usuario/registro-usuario/registro-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registrar-repartidor', component: RegistrarRepartidorComponent},
+  { path: 'nuevo-producto', component: NuevoProductoComponent},
+  { path: 'registro-usuario', component: RegistroUsuarioComponent},
+  { path: '', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/repartidor/repartidor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrarRepartidorComponent } from './registrar-repartidor/registrar-repartidor.component';
+
+
+
+@NgModule({
+  declarations: [RegistrarRepartidorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RepartidorModule { }
