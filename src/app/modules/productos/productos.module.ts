647f87626a26e68a06f5fce56abb7d99742dbcc7
ivan <irmm2001@hotmail.com>
2020-11-13 20:10:33 -0500

    Modulo Productos
    se genero el código base de productos(tiene error pero no se que sera)

---
+++ src/app/modules/productos/productos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NuevoProductoComponent } from './nuevo-producto/nuevo-producto.component';
+
+
+
+@NgModule({
+  declarations: [NuevoProductoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductosModule { }
